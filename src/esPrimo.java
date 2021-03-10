4755a0fe2b8e533f724d7637c72bc4d78629a5ea
crubioa <55598627+car1803@users.noreply.github.com>
2021-03-10 18:04:41 -0500

    Update esPrimo.java

--- src/esPrimo.java
+++ src/esPrimo.java
@@ -1,12 +1,10 @@
 public class esPrimo(int numero){
-    public boolean primo(int numero){
-        boolean primo = true;
-        for(I = 2; I < n; I++){
-        if (numero % I == 0){
-            primo=false;
-            break;
-            }
-        return primo;   
+    boolean primo = true;
+    for(I = 2; I < n; I++){
+    if (numero % I == 0){
+        primo=false;
+        break;
         }
+    return primo;   
     }
 }
