63fd76812e22ece829a7bfa4f76f499befac6e0b
crubioa <55598627+car1803@users.noreply.github.com>
2021-03-10 17:46:30 -0500

    Create esPrimo.java

---
+++ src/esPrimo.java
@@ -0,0 +1,10 @@
+public class esPrimo(int numero){
+  boolean primo = true;
+  for(I = 2; I < n; I++){
+    if (numero % I == 0){
+        primo=false;
+        break;
+    }
+    return primo;   
+  }
+}
