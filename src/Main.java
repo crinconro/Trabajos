581a8cbb935c78b9f19a6669f2cdf6c57046310f
Nicolás V <expectopatrom@gmail.com>
2021-03-10 16:41:46 -0500

    Main class added and fibonacci feature (#4)
    * Main class added and fibonacci feature
    
    * .idea folder deleted

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String args[]){
+        int nums[] = {1,2,3,4,5};
+        promedio avg = new promedio(nums);
+        System.out.println(avg.IntPromedio());
+        calculadora calc = new calculadora();
+        for(int i=0; i<10; i++){
+            System.out.println(calc.fibonacci(i));
+        }
+    }
+}
