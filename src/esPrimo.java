15b788f903340687f0d330dc800f8e90797ff5ff
crinconro <robayo_cesar@hotmail.com>
2021-03-11 23:45:07 -0500

    prototipo-1

--- src/esPrimo.java
+++ src/esPrimo.java
@@ -1,12 +1,13 @@
-public class esPrimo(){
+public class esPrimo{
     public boolean primo(int numero){
         boolean primo = true;
-        for(I = 2; I < n; I++){
+        for(int I = 2; I < numero; I++){
         if (numero % I == 0){
             primo=false;
             break;
             }
-        return primo;   
+          
         }
+        return primo; 
     }
 }
